5dfbad54ad266c7dbaf85c6455e158ed5fa40ad1
Danny Gershman <dgershman@securityscorecard.io>
2017-04-28 23:06:31 -0400

    adding simple way to use smart card across multiple devices

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,12 @@
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/.atom ~/.atom
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+git pull && git submodule init && git submodule update && git submodule status
+cp -R custom-themes/*.zsh-theme .oh-my-zsh/themes/
 ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+brew install gnupg gpg2
+gpg2 --recv-keys 7D2E1496
+gpg2 --card-status
+gpg2 --list-secret-keys
+
+echo "Install GPGSuite for macOS https://gpgtools.org/"
