4e6770c05cf4d2308a98b50f40daedb33933206f
Danny Gershman <dgershman@securityscorecard.io>
2017-05-07 16:20:53 -0400

    final changes to make a stub

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,9 @@ brew install gnupg gpg2
 gpg2 --recv-keys 7D2E1496
 gpg2 --card-status
 gpg2 --list-secret-keys
+echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf
+pkill ssh-agent ; pkill gpg-agent ; \
+  eval $(gpg-agent --daemon --enable-ssh-support --use-standard-socket \
+  --log-file ~/.gnupg/gpg-agent.log --write-env-file)
 
 echo "Install GPGSuite for macOS https://gpgtools.org/"
