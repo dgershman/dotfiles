0c18f661ec7a8ac69f94b9600027609f90d6d838
Danny Gershman <dgershman@securityscorecard.io>
2017-05-11 15:01:30 -0400

    adding notebooks

--- jupyter.sh
+++ jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --rm -p 8787:8888 jupyter/datascience-notebook
+docker run -it --rm -v ~/.dotfiles/jupyter-notebooks:/home/jovyan/work -p 8787:8888 jupyter/datascience-notebook
