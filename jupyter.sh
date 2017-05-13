d5727bb2ae810f2d43404c9a27f472f4dedeed09
Danny Gershman <dgershman@securityscorecard.io>
2017-05-12 20:35:21 -0400

    adding abilities to add modules to jupter, alfred workflow for starting jupyter

--- jupyter.sh
+++ jupyter.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run -it --rm -v ~/.dotfiles/jupyter-notebooks:/home/jovyan/work -p 8787:8888 jupyter/datascience-notebook
+docker rm -f jupyter
+docker run -d --name=jupyter -v ~/.dotfiles/jupyter-notebooks:/home/jovyan/work -p 8787:8888 jupyter/datascience-notebook
+docker exec -it jupyter /bin/sh "/home/jovyan/work/install-requirements.sh"
