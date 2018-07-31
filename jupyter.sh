7c818dc76ae1324fb1f91d17bf3853c5c5a02553
Danny Gershman <dgershman@securityscorecard.io>
2018-07-31 17:59:49 -0400

    switching to port 8888

--- jupyter.sh
+++ jupyter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker rm -f jupyter
-docker run -d --name=jupyter -v ~/.dotfiles/jupyter-notebooks:/home/jovyan/work -p 8787:8888 jupyter/datascience-notebook
+docker run -d --name=jupyter -v ~/.dotfiles/jupyter-notebooks:/home/jovyan/work -p 8888:8888 jupyter/datascience-notebook
 docker exec -it jupyter /bin/sh "/home/jovyan/work/install-requirements.sh"
